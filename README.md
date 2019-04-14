532ea3cd7cb6e34185e1f4e11e735862fca3116f
Cullan Shewfelt <cullanrocks@gmail.com>
2019-04-13 19:18:44 -0700

    initializing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Metadata-Tagging
+An application that tags audio metadata
---
+++ src/components/Dashboard.js
@@ -0,0 +1,45 @@
+import React, {Component, lazy, Suspense} from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import ReactModal from 'react-modal';
+import Loader from './SubComponents/Loader';
+import {initializeData} from '../actions/dataActions';
+
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='dashboard'>
+          <div className='container'>
+            Hello
+          </div>
+
+        </div>
+      </div>
+    )}
+  }
+
+const mapStateToProps = (state) => {
+  return {
+    data: state.data
+  }
+}
+
+const mapDispatchToProps = {
+  initializeData
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
