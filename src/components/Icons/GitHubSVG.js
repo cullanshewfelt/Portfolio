f57e6a4365ab984ff95ac09fd4fc307d1558312f
Cullan Shewfelt <cullanrocks@gmail.com>
2019-05-13 09:20:04 -0700

    renaming back to githubsvg

--- src/components/Icons/GitHubSVG.js
+++ src/components/Icons/GitHubSVG.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const GithubSVG = (props) => {
+const GitHubSVG = (props) => {
   const { backFill, className, mainFill } = props.props;
   return(
     <svg
@@ -37,4 +37,4 @@ const GithubSVG = (props) => {
   )
 }
 
-export default GithubSVG;
+export default GitHubSVG;
