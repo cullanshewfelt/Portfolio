0d75bd17379a5aa532e47983e7ae057d52e9b4f2
Cullan Shewfelt <cullanrocks@gmail.com>
2019-05-17 17:29:44 -0700

    production build not reloading on all endpoints. changing server.js to serve on all paths.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ util.inspect.defaultOptions.maxArrayLength = null;
 
 app.use(express.static(path.join(__dirname, 'public/build')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
